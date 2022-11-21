f177d36392c0f6b018adce60b117a08061bc84ef
Andrewwodres <118737010+Andrewwodres@users.noreply.github.com>
2022-11-21 15:33:47 +0300

    Add files via upload

---
+++ tester.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import sqlite3
+
+con = sqlite3.connect("finans_piromida.db")
+
+# Создание курсора
+cur = con.cursor()
+
+# Выполнение запроса и получение всех результатов
+result = cur.execute("""SELECT * FROM Andrcoins""").fetchall()
+# Вывод результатов на экран
+
+print("Content-type: text/html")
+print()
+print('<h1 align="center">Andrcoins owners</h1>')
+for elem in result:
+    print(f"<h2 align='center'>{': '.join(map(str, elem))}</h2>")
+con.close()
