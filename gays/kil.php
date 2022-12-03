bf14eca360bcaa95044b431c6d8ba151320e89a1
Andrewwodres <118737010+Andrewwodres@users.noreply.github.com>
2022-12-03 23:16:26 +0300

    Update kil.php

--- gays/kil.php
+++ gays/kil.php
@@ -5,11 +5,14 @@
 	$info = []
 	
 	if ($query = $db->query(statement:"SELECT * FROM distime")){
-		$query->fetchAll();
+		$info = $query->fetchAll(fetch_style: PDO::FETCH_ASSOC);
+	} else{
+		print_r($db->errorinfo());
 	}
 
 ?>
 
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
@@ -18,12 +21,31 @@
     <script defer src="https://pyscript.net/latest/pyscript.js"></script>
 </head>
 <body>
-    <table>
+	<?php foreach ($info as $data): ?>
+		<div class ="text">
+			<text><?php echo $data['dayid']?>
+			<text><?php echo $data['massa']?>
+			<text><?php echo $data['timeup']?>
+			<text><?php echo $data['timedown']?>
+	<?php endforeach; ?>
+    <table border="1" align="center" width="85%">
 		<tr>
-	<td>номер дня</td>
-	<td>вес</td>
-	<td>время подъема</td>
-	<td>время в кровать</td>
+	<th>номер дня</th>
+	<th>вес</th>
+	<th>время подъема</th>
+	<th>время в кровать</th>
+	</tr>
+		<tr>
+	<td>0</td>
+	<td>53</td>
+	<td>12/02/2022, 8:24:34</td>
+	<td>12/02/2022, 23:54:44</td>
+	</tr>
+		<tr>
+	<td>1</td>
+	<td>55</td>
+	<td>12/03/2022, 10:12:24</td>
+	<td>12/03/2022, 23:12:24</td>
 	</tr>
 	</table>
 	
